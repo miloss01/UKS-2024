7d1effd0d06b7a34d58bfe22eeaa2ecf1fdb384e
vdevic01 <vlada.devic.2001@gmail.com>
2024-12-06 15:07:57 +0100

    Added db connection

--- DockerHubBackend/DockerHubBackend/Program.cs
+++ DockerHubBackend/DockerHubBackend/Program.cs
@@ -1,6 +1,12 @@
+using DockerHubBackend.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
