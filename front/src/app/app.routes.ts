1a36b6c3823ac207d95583100beb7bccf04ef454
vdevic01 <vlada.devic.2001@gmail.com>
2024-12-13 23:34:22 +0100

    Update app.routes.ts

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './modules/layout/landing-page/landing-page.component';
+import {LoginPageComponent} from "./modules/layout/login-page/login-page.component";
 
 export const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
   { path: '**', component: LandingPageComponent }
 ];
