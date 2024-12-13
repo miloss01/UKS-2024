2c37c432cfc7ff1f4626b4f26ea182fa44f4e3d4
vdevic01 <vlada.devic.2001@gmail.com>
2024-12-13 23:34:44 +0100

    Updated providers

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {BrowserModule} from "@angular/platform-browser";
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(BrowserModule),
+    provideAnimations(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
 };
