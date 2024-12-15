fdb28a9f641e4f6cfb32164b97df5a12513de56c
miloss01 <mikicamiki.bat@gmail.com>
2024-12-15 22:23:46 +0100

    Create explore section on frontend

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './modules/layout/landing-page/landing-page.component';
 import {LoginPageComponent} from "./modules/layout/login-page/login-page.component";
 import {HomePageComponent} from "./modules/layout/home-page/home-page.component";
+import {ExplorePageComponent} from "./modules/layout/explore-page/explore-page.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   { path: 'home', component: HomePageComponent },
+  { path: 'explore', component: ExplorePageComponent },
   { path: '**', component: LandingPageComponent }
 ];
