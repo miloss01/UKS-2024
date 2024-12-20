fc355a33be847933ea662397003ad044b5b7180e
miloss01 <mikicamiki.bat@gmail.com>
2024-12-20 11:45:04 +0100

    Init component for setting badges to users

---
+++ front/src/app/modules/layout/user-badges/user-badges.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-badges',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-badges.component.html',
+  styleUrl: './user-badges.component.css'
+})
+export class UserBadgesComponent {
+
+}
